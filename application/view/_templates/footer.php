c93707afc1931a9bc0c73efebb51784038d06327
unknown <vishal.141414@gmail.com>
2017-05-04 01:35:08 +0530

    Initail commit Of Php Panique Mini Mvc

---
+++ application/view/_templates/footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </footer>
+<!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
+    <script src="<?php echo URL; ?>js/jquery1.12.4.min.js"></script>
+
+    <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
+    <script>
+        var url = "<?php echo URL; ?>";
+    </script>
+
+    <!-- our JavaScript -->
+    <script src="<?php echo URL; ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo URL; ?>js/application.js"></script>
+</body>
+</html>
